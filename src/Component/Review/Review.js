e8c94a38208420f6b6dcd3cd0e8878bb434bdf92
sayman <mdrabbi.asm@gmail.com>
2022-04-04 01:51:55 +0600

    review section done

--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Review.css'
 const Review = ({ review }) => {
-    console.log(review)
+    const { Picture,name,about} = review;
     return (
         <div className=' mx-auto review-container col-6 md:col-4'>
-            
+            <img src={Picture} className="d-flex mx-auto mt-3" style={{ width: "150px", height: "150px", borderRadius: "50%" }} alt="" />
+            <h5 className='text-center mt-3'>Name: {name}</h5>
+            <p>{`${about.slice(0,90)+'....'}`}</p>
         </div>
     );
 };
