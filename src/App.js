45741d15f8ee32dd03e1e9b43f8ceefcd01fa6e6
sayman <mdrabbi.asm@gmail.com>
2022-04-03 23:58:57 +0600

    setup all route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Component/Header/Header';
 import Hero from './Component/Hero/Hero';
+import { Route, Routes } from 'react-router-dom';
+import Reviews from './Component/Reviews/Reviews';
+import Dashboard from './Component/Dashboard/Dashboard';
+import Blogs from './Component/Blogs/Blogs';
+import About from './Component/About/About';
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Hero></Hero>
+      <Header></Header>      
+      <Routes>
+        <Route path='/' element={<Hero></Hero>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css'
 const Header = () => {
     return (
         <>
-            <Navbar bg="white" variant="dark" className='py-3 '>
+            <Navbar variant="dark" className='py-3 custom-navbar'>
           <Container>
-          <Navbar.Brand href="#home" className='text-dark fs-3 fw-bold d-none d-md-block'>HPHONE</Navbar.Brand>
-    <Nav className="ms-auto fs-5">
-      <Nav.Link href="#home" className='text-dark'>Home</Nav.Link>
-      <Nav.Link href="#features"className='text-dark'>Reviews</Nav.Link>
-      <Nav.Link href="#pricing"className='text-dark'>Dashboard</Nav.Link>
-      <Nav.Link href="#pricing"className='text-dark'>Blogs</Nav.Link>
-      <Nav.Link href="#pricing"className='text-dark'>About</Nav.Link>
+          <Navbar.Brand href="#home" className='text-white fs-3 fw-bold d-none d-md-block'>HPHONE</Navbar.Brand>
+            <Nav className="ms-auto fs-5">
+            <Link to="/"className='text-white text-decoration-none me-3'>Home</Link>
+      <Link to="/reviews"className='text-white text-decoration-none me-3'>Reviews</Link>
+      <Link to="/dashboard"className='text-white text-decoration-none me-3'>Dashboard</Link>
+      <Link to="/blogs"className='text-white text-decoration-none me-3'>Blogs</Link>
+      <Link to="/about"className='text-white text-decoration-none me-3'>About</Link>
     </Nav>
     </Container>
   </Navbar>
