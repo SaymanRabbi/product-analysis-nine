f3eb7be55f33d55edb5ec30cd8e6aedb41336a12
sayman <mdrabbi.asm@gmail.com>
2022-04-04 13:34:35 +0600

    user rating

--- src/Component/HomeReview/HomeReview.js
+++ src/Component/HomeReview/HomeReview.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {FaStar} from "react-icons/fa"
 
 const HomeReview = ({review}) => {
-    const {Picture,name,about,country}=review
+    const { Picture, name, about, country} = review;
+   
     return (
         <div className=' mx-auto review-container col-6 md:col-4 pt-1'>
             <div className='d-flex justify-content-between mb-2 '>
                 <div className='d-flex align-items-center'>
                     <img src={Picture} style={{ width: "50px", height: "50px",borderRadius:"50%"}} alt="" />
                     <div className='ms-2'>
-                    <span>Start</span>
+                        {
+                            [...Array(5)].map(()=> {
+                                return <FaStar key={Math.random()} className="text-warning"></FaStar>
+                            })
+                    }
                     <h6>{name}</h6>
                     </div>
                 </div>
--- src/Component/Review/Review.js
+++ src/Component/Review/Review.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import './Review.css'
+import './Review.css';
+import {FaStar} from "react-icons/fa"
 const Review = ({ review }) => {
-    const { Picture,name,about} = review;
+    const { Picture, name, about, country} = review;
+    // const number = parseInt(rating)
+   
     return (
-        <div className=' mx-auto review-container col-6 md:col-4'>
-            <img src={Picture} className="d-flex mx-auto mt-3" style={{ width: "150px", height: "150px", borderRadius: "50%" }} alt="" />
-            <h5 className='text-center mt-3'>Name: {name}</h5>
-            <p>{`${about.slice(0,90)+'....'}`}</p>
+        <div className=' mx-auto review-container col-6 md:col-4 pt-1'>
+            <div className='d-flex justify-content-between mb-2 '>
+                <div className='d-flex align-items-center'>
+                    <img src={Picture} style={{ width: "50px", height: "50px",borderRadius:"50%"}} alt="" />
+                    <div className='ms-2'>
+                        {
+                            [...Array(5)].map(()=> {
+                                return <FaStar key={Math.random()} className="text-warning"></FaStar>
+                            })
+                    }
+                    <h6>{name}</h6>
+                    </div>
+                </div>
+               <img src={country} style={{height:"20px",width:"20px",borderRadius:"50%"}} alt="" />
+            </div>
+            <small > <i>{`${about.slice(0,97)}...`}</i> </small>
         </div>
     );
 };
